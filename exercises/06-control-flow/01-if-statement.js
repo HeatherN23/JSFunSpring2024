9268a716bc2b88f9a54de1fefb525a1d00e32388
Heather Nygaard <hmnygaard2@gmail.com>
2024-02-28 17:49:44 -0500

    Exercises 5 & 6 Completed

--- exercises/06-control-flow/01-if-statement.js
+++ exercises/06-control-flow/01-if-statement.js
@@ -17,3 +17,8 @@ let isBuyingDrink = true; // e.g.
  */
 
 // WRITE YOUR ANSWER BELOW THIS LINE
+if (isBuyingDrink){
+  console.log(total += drink);
+} else {
+  console.log(total);
+}
--- exercises/06-control-flow/02-if-statement.js
+++ exercises/06-control-flow/02-if-statement.js
@@ -13,3 +13,11 @@ let total;
  */
 
 // WRITE YOUR ANSWER BELOW THIS LINE
+
+if ( tip ){
+  total=subtotal + subtotal*tip;
+  console.log(total);
+} else {
+  total=subtotal;
+  console.log(total);
+}
