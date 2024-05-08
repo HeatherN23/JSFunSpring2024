ed4b0c090abacfef7bf79fe9cb3d482639dcbfd9
Heather Nygaard <hmnygaard2@gmail.com>
2024-05-08 17:15:28 -0400

    Exercise 31 complete

--- exercises/31-async-await/async-await.js
+++ exercises/31-async-await/async-await.js
@@ -8,4 +8,22 @@
    * This is the API you will be using. The method will be GET.
    * https://ron-swanson-quotes.herokuapp.com/v2/quotes
    */
-})();
+
+  
+  const btn = document.querySelector(".btn").addEventListener('click', updateQuote => {
+    
+    axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes')     // get the quote from the url
+      .then( response => {                                              // response is the GET returned in object form
+          const newQuote = response.data;                               // data of the response object
+          const quote = document.querySelector("#quote");               // get the quote element
+          quote.textContent = newQuote;                                 // display the new quote.
+        });     
+      })
+      .catch( function( error ) {
+          console.log(error);
+      });     
+
+  })();
+  
+  
+
